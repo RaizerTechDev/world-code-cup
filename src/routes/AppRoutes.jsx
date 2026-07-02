610ee95058f97f8e448773336b8963ae26dbeda3
conversormoedas <conversormoedas44@gmail.com>
2026-07-02 13:29:58 -0300

    :sparkles: feat: adiciona rota da página de ranking

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Regulamento from '../pages/Regulamento/Regulamento';
-// import Ranking from '../pages/Ranking/Ranking';
+import Ranking from '../pages/Ranking/Ranking';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/regulamento" element={<Regulamento />} />
-      {/* <Route path="/ranking" element={<Ranking />} /> */}
+      <Route path="/ranking" element={<Ranking />} />
     </Routes>
   );
 };
