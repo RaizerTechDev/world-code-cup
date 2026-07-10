c7618743dadf7498b2d658f2bfc54b8d193e40d5
conversormoedas <conversormoedas44@gmail.com>
2026-07-10 12:00:53 -0300

    :wrench: refactor: reorganiza estrutura principal da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppRoutes from './routes/AppRoutes';
 
+import './styles/global.scss'; // Importe seu CSS global aqui
+
 function App() {
   return (
     <BrowserRouter>
